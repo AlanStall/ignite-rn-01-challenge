c94ddeeccc164c7a8f5c96c152c66e41a06fc3ab
Alan Stall <alan_kstall@hotmail.com>
2023-04-18 23:23:14 -0300

    style: finish design home screen

--- src/screen/Home/index.tsx
+++ src/screen/Home/index.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from "react-native";
+
+import NoTasks from '../../components/NoTasks';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 /* import Logo02 from '../../assets/logo.png'; */
 /* import Logo from '../../assets/favicon.png'; */
@@ -8,63 +18,159 @@ import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from "reac
 export default function Home() {
   return (
     <>
-      <View
-        style={{
-          backgroundColor: "#0D0D0D",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          padding: 40,
-        }}
-      >
+      <View style={styles.headerLogo}>
         <StatusBar style="auto" />
         <Image
           source={require("../../assets/logo.png")}
-          style={{ margin: 40 }}
+          style={{ margin: 10 }}
         />
       </View>
-      <View
-        style={{
-          backgroundColor: "#1A1A1A",
-          alignItems: "center",
-          justifyContent: "flex-start",
-          padding: 30,
-          flexDirection: 'row',
-        }}
-      >
-        <TextInput style={styles.input} />
-        <TouchableOpacity style={{top: -70, marginLeft: 10 }}><Text style={{color: 'white'}}>{'BOTÃO'}</Text></TouchableOpacity>
-      </View>
-      <View
-        style={{ flex: 1, backgroundColor: "#1A1A1A", alignItems: "center" }}
-      >
-        <Text style={{ color: "white" }}>
-          Open up App.tsx to start working on your app!
-        </Text>
-      </View>
+      <View style={styles.container}>
+        <View style={styles.wrapInput}>
+          <TextInput
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor="#808080"
+            style={styles.input}
+          />
+
+          <View>
+            <TouchableOpacity style={styles.buttonBehind}></TouchableOpacity>
+            <TouchableOpacity style={styles.buttonAdd}>
+              <Image
+                source={require("../../assets/plus.png")}
+                style={{ width: 16, height: 16 }}
+              />
+            </TouchableOpacity>
+          </View>
+          
+        </View>
+
+        <View style={styles.followUpActions}>
+          <View style={styles.wrapCreatedTasks}>
+            <Text style={styles.createdTasksTexts}>{"Criadas"}</Text>
+            <View style={styles.wrapNumberTasksCreated}>
+              <Text style={styles.numberTasksCreated}>{0}</Text>
+            </View>
+          </View>
+          <View style={styles.wrapCompletedTasks}>
+            <Text style={styles.completedTasksTexts}>{"Concluídas"}</Text>
+            <View style={styles.wrapNumberTasksCompleted}>
+              <Text style={styles.numberTasksCompleted}>{0}</Text>
+            </View>
+          </View>
+        </View>
+
+        <NoTasks/>
+
+      </View>      
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  headerLogo: {
     backgroundColor: "#0D0D0D",
+    height: 173,
     alignItems: "center",
-    justifyContent: "flex-start",
-    padding: 40,
+    justifyContent: "center",
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#1A1A1A",
+  },
+  wrapInput: {
+    top: -54,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingLeft: 24,
+    paddingRight: 24,
   },
   input: {
     flex: 1,
-    top: -70,
     backgroundColor: "#262626",
     width: "100%",
-    height: 56,
+    height: 54,
     borderRadius: 5,
-    color: "#FFF",
+    color: "#F2F2F2",
     fontSize: 16,
     padding: 10,
-    marginBottom: 0,    
-    borderWidth: 2, 
-    borderColor: 'black', 
-    borderStyle: 'solid'
+    marginBottom: 0,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderStyle: "solid",
+    marginRight: 4,
+  },
+  buttonAdd: {
+    width: 52,
+    height: 52,
+    backgroundColor: "#1E6F9F",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 6,
+    bottom: 27,
+  },
+  buttonBehind: {
+    width: 52,
+    height: 52,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 6,
+    top: 25,
+  },
+  followUpActions: {    
+    flexDirection: 'row',
+    top: -48,
+    justifyContent: 'space-between',
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  wrapCreatedTasks: {
+    flexDirection: 'row',
+  },
+  createdTasksTexts: {
+    color: '#4EA8DE',
+    paddingRight: 8,    
+    fontSize: 14,
+    fontWeight: '800',
+    justifyContent: 'center',
+  },
+  wrapNumberTasksCreated: {
+    backgroundColor: '#333333',
+    width: 25,
+    height: 19,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  numberTasksCreated: {
+    color: '#fff',
+    fontSize: 12,
+    fontWeight: '800',
+  },
+  wrapCompletedTasks: {
+    flexDirection: 'row',
+  },
+  completedTasksTexts: {
+    color: '#8284FA',
+    paddingRight: 8,    
+    fontSize: 14,
+    fontWeight: '800',
+    justifyContent: 'center',
+  },
+  wrapNumberTasksCompleted: {
+    backgroundColor: '#333333',
+    width: 25,
+    height: 19,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  numberTasksCompleted: {
+    color: '#fff',
+    fontSize: 12,
+    fontWeight: '800',
   },
 });
